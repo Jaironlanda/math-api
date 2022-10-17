d67f4c274089545f053b57a9dd7783dfc16a6103
Jairon Landa <jaironlanda@yahoo.com>
2022-10-17 11:06:30 +0800

    add middleware cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from urllib.request import urlopen
 from random import sample
 import ijson
@@ -6,6 +7,19 @@ import ijson
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 async def loadJson(q_lvl: str, q_op: str = None):
     file = urlopen(
